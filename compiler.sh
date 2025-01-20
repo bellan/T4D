28299cdb31c787fa36fda28e0b1ebd8a09aaac7d
eleoracca <28921698+eleoracca@users.noreply.github.com>
2025-01-20 14:54:18 +0100

    Added some comments and compilation options for different number of threads

--- compiler.sh
+++ compiler.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# ~~~ Command to execute the first time
 setup() {
+    # --- Creating useful directories
     if [ ! -d "./build" ]; then
         mkdir -p "./build"
     fi
@@ -12,38 +14,64 @@ setup() {
     fi
 }
 
+# ~~~ Compiling and running the code
 compile_run() {
+    # --- Compiling the code
     cd build
     echo "Creating make file"
     cmake ..
     echo "Compiling"
-    make -j 4
+
+    # Adjusting the compilation to the numer of threads available
+    #make -j 4
+    num_threads=$(($(nproc) -1))
+    if [ "$num_threads" -lt 1 ]; then
+        num_threads=1
+    fi
+    
+    make -j $num_threads
+
+    # --- Executing the code
     echo "Executing"
     ./Tracking_simulation
 }
 
+# ~~~ Compiling and running the code and showing the results
 compile_run_show() {
+    # --- Compiling the code
     cd build
     echo "Creating make file"
     cmake ..
     echo "Compiling"
-    make -j 4
+
+    # Adjusting the compilation to the numer of threads available
+    num_threads=$(($(nproc) -1))
+    if [ "$num_threads" -lt 1 ]; then
+        num_threads=1
+    fi
+    make -j $num_threads
+
+    # --- Executing the code
     echo "Executing"
     ./Tracking_simulation
 
+    # --- Visualisation of data and results
     echo "
 
     "
-    echo "Creating data visualization"
+    echo "Creating data visualisation"
     cd ../DataVisualization
     python dataVis.py
     cd ..
 }
 
+# ~~~ Visualization of data and results
 show() {
-    echo "Creating data visualization"
+    # --- Visualisation of data and results
+    echo "Creating data visualisation"
     cd DataVisualization
     python dataVis.py
     cd ..
 }
+
 $1
