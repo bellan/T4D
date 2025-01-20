70d8b65cca67d65c72e1da0a7b2cda0ff2b7ae37
eleoracca <28921698+eleoracca@users.noreply.github.com>
2025-01-20 17:16:48 +0100

    Added some comments and elapsed time

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
+// Interfaces
 #include "PhysicalParameters.hpp"
 #include "Simulation.hpp"
+#include "Utils.hpp"
+
+// Other libraries
+#include <ctime> 
+
+// Namespaces
+using namespace std;
+using namespace Utils;
 
 int main() {
+  // --- Execution time
+  cout << "\n--------------------------------------------------------------------------" << endl;
+  auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  cout << " Beginning simulation at: " << ctime(&now) << endl;
+  float begintime = ((float)clock())/CLOCKS_PER_SEC;
 
+  // --- Simulation
   auto simulation = Simulation();
   simulation.runSimulation(NUMBER_OF_PARTICLES);
-  /*simulation.testDetector(NUMBER_OF_PARTICLES, 5);*/
-  /*simulation.testDetector(1, 5);*/
+
+  // --- Simulation of layers testing
+  //simulation.testDetector(NUMBER_OF_PARTICLES, 5);
+  //simulation.testDetector(1, 5);
+
+  // --- Execution time
+  now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  cout << "\n Finished analysis at: " << ctime(&now);
+  float endtime = ((float)clock())/CLOCKS_PER_SEC;
+  Utils::printTime(begintime, endtime);
+  cout << "\n--------------------------------------------------------------------------" << endl;
 
   return 0;
 }
