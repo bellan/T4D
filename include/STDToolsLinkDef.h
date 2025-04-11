8d276b0cd4f0b6dcf3311c1601cd63f9b04204fc
eleoracca <28921698+eleoracca@users.noreply.github.com>
2025-04-11 18:41:09 +0200

    added the linking files for the custom dictionary

---
+++ include/STDToolsLinkDef.h
@@ -0,0 +1,14 @@
+#include "ParticleGun.hpp"
+#include "ParticleState.hpp"
+#include <vector>
+
+#ifdef __CLING__
+
+// Data format classes
+#pragma link C++ class  ParticleGun;
+#pragma link C++ class  ParticleState;
+
+// Vectors of data format classes
+#pragma link C++ class  vector<ParticleState>;
+
+#endif
